4152dd33fbaf62d577b70a91cbb1c3ee24e0183f
h1tok1r11 <127687083+h1tok1r11@users.noreply.github.com>
2023-03-24 11:37:32 +0300

    Add files via upload

--- practice_3.cpp
+++ practice_3.cpp
@@ -1,10 +1,10 @@
 ﻿#include <iostream>
-#include <windows.h>
-#include <string.h>
-#include <iomanip>
-#include <stdlib.h>
-
+#include <Windows.h>
+#include <string>
+#include <math.h>
 using namespace std;
+
+#define N 95
 
 struct Date
 {
@@ -13,164 +13,173 @@ struct Date
     unsigned short year;
 };
 
-//struct Record
-//{
-//    char surname[20];
-//    char code;
-//    float count;
-//    unsigned short place;
-//    struct Date date;
-//};
-
-struct Record {
-    int info;
-};
-
-struct Node {
-    Record info;
-    Node* next_item;
-    Node* prev_item;
-};
-Node* head; Node* tail;
-int cnt_of_items = 0;
-
-void insert_at_head(Record info) {
-    Node* new_item = new Node();
-    new_item->info = info;
-    if (cnt_of_items == 0)
-        new_item->next_item = new_item->prev_item = NULL;
-    else {
-        new_item->next_item = head;
-        head->prev_item = new_item;
-    }
-    head = new_item;
-    cnt_of_items++;
-}
-
-void insert_at_tail(Record info) {
-    Node* new_item = new Node();
-    new_item->info = info;
-    if (cnt_of_items == 0)
-        new_item->next_item = new_item->prev_item = NULL;
-    else {
-        new_item->prev_item = tail;
-        tail->next_item = new_item;
-        cnt_of_items++;
-    }
-}
-
-void insert_item(int idx, Record info) {
-    if (not (idx >= 0 and idx <= cnt_of_items and cnt_of_items >= 0)) {
-        cout << "Try again" << endl;
-        return;
-    }
-    if (idx == 0)
-        insert_at_head(info);
-    else {
-        Node* cur_item = head;
-        for (int i = 0; i < idx - 1; i++) {
-            cur_item = cur_item->next_item;
-        }
-        Node* aft_item = cur_item->next_item;
-        Node* new_item = new Node();
-        new_item->info = info;
-        new_item->next_item = aft_item; aft_item->prev_item = new_item;
-        cur_item->next_item = new_item; new_item->prev_item = cur_item;
-        cnt_of_items++;
-    }
-
-}
-
-void find_item(int value) {
-    int idx = 0; Node* cur_item = head;
-    while (cur_item->info != value)
-}
-
-
-void myCentr(string s, int wLine) {
-
-    unsigned short w = s.length();
-    int delta = (wLine - w) / 2;
-    cout << left;
-    cout.width(delta); cout << " ";
-    cout << s;
-    cout.width(delta + 1); cout << " ";
-}
-
-void Printdate(unsigned short day, unsigned short month, unsigned short year, int wLine)
+struct Record
 {
-    int w = 10;
-    int delta = (wLine - w) / 2 - 1;
+    char product[15];
+    char type[2];
+    double price;
+    unsigned short min;
+    struct Date date;
+};
+
+struct node
+{
+    Record data;
+    node* next;
+};
+node* head;
+int countItem;
+
+void dash(unsigned short n)
+{
+    cout.fill('-'); cout.width(n); cout << "-" << endl; cout.fill(' ');
+}
+
+void center(string s, int lineLen)
+{
+    int w = s.length();
+    int delta = (lineLen - w) / 2;
     cout << left;
-    cout.width(delta); cout << " ";
-    if (day > 9) {
-        cout << day;
-    }
-    else {
-        cout << "0" << day;
-    }
-    cout << ".";
-    if (month > 9) {
-        cout << month;
-    }
-    else {
-        cout << "0" << month;
-    }
-    cout << ".";
+    cout.fill(' ');
+    cout.width(delta); cout << ' ' << s;
+    cout.width(delta); cout << ' ';
+}
+
+void drawDate(unsigned short day, unsigned short month, unsigned short year, unsigned short w)
+{
+    cout.width((w - 10) / 2); cout << "";
+
+    if (day < 10) { cout << "0" << day << "."; }
+    else { cout << day << "."; }
+
+    if (month < 10) { cout << "0" << month << "."; }
+    else { cout << month << "."; }
+
     cout << year;
-    cout.width(delta);
-    cout << " ";
+    int t = (w - 10) / 2 + 1;
+    cout.width(t); cout << right << "|";
 }
 
-void border(int width) {
-    cout.width(width); cout.fill('-'); cout << "-" << endl;
-}
-
-void Table(struct Record* records)
+void addItem(Record data)
 {
-    int width = 80;
-    int datespace = 9;
-    cout << endl; border(width + 1);
-    cout.width(width); cout.fill(' '); cout << left << "|Ведомость спортивных состязаний"; cout << "|" << endl;
-    border(width + 1);
-    cout.fill(' ');
-    cout << left << "|Фамилия участника";
-    cout << left << "|Код команды";
-    cout << left << "|Количество балов";
-    cout << left << "|Место в итоге";
-    cout << "|";  myCentr("Дата выступления", 12); cout << "|" << endl;
-    cout.fill(' ');
-    border(width + 1);
-    cout.fill(' ');
-    for (int i = 0; i < 3; i++) {
-        cout << left << "|"; cout.width(17); cout << left << records[i].surname;
-        cout << left << "|"; cout.width(11); cout << left << records[i].code;
-        cout << left << "|"; cout.width(16); cout << left << records[i].count;
-        cout << left << "|"; cout.width(13); cout << left << records[i].place;
-        cout << left << "|";
-        Printdate(records[i].date.day, records[i].date.month, records[i].date.year, 20);
-        cout << "|" << endl;
+    node* newItem = new node;
+    newItem->data = data;
+    if (countItem == 0)
+    {
+        head = newItem;
+        head->next = head;
     }
-    border(width + 1);
-    cout.fill(' '); cout.width(width - 1); cout << left << "| Примечание: Д - \"Динамо\", С - \"Спартак\", Ш - \"Шахтер\""; cout << " | " << endl;
-    border(width + 1);
+    else
+    {
+        node* current = head;
+        for (int i = 0; i < (countItem - 1); i++)
+        {
+            current = current->next;
+        }
+        newItem->next = head;
+        current->next = newItem;
+    }
+    countItem++;
 }
 
-void Rusakov(struct Record* records) {
-    char src[8] = "Русаков";
-    for (int i = 0; i < 3; i++) {
-        strcpy_s(records[i].surname, 10, src);
+void insertItem(int index, Record data)
+{
+    if (index < 1) { index = countItem + index; }
+    else if (index > countItem) { index = abs(index % countItem); }
+    index == 0 ? index = countItem : index;
+
+    if (index == 1)
+    {
+        node* newItem = new node;
+        newItem->data = data;
+        newItem->next = head;
+        head = newItem;
+    }
+    else
+    {
+        node* newItem = new node;
+        newItem->data = data;
+        node* current = head;
+        for (int i = 1; i < (index - 1); i++)
+        {
+            current = current->next;
+        }
+        newItem->next = current->next;
+        current->next = newItem;
+    }
+    countItem++;
+}
+
+void insertMonth()
+{
+    node* current = head;
+    Record newRecords = { "Степлер","К",50.99,1,{13,7,2023} };
+    int temp = countItem;
+    int fix = 0;
+    for (int i = 0; i < temp; i++)
+    {
+        if (newRecords.date.month == current->data.date.month)
+        {
+            insertItem(i + 1 + fix, newRecords);
+            fix++;
+        }
+        current = current->next;
     }
 }
 
-void Input(struct Record* records) {
-    for (int i = 0; i < 3; i++) {
-        cout << "Input the " << i + 1 << " line" << endl << endl;
-        cout << i + 1 << " participant's surname:"; cin >> records[i].surname;
-        cout << i + 1 << " code of team:"; cin >> records[i].code;
-        cout << i + 1 << " count of points:"; cin >> records[i].count;
-        cout << i + 1 << " place in the end:"; cin >> records[i].place;
-        cout << i + 1 << " performance date:"; cin >> records[i].date.day >> records[i].date.month >> records[i].date.year; cout << endl;
+void drawHat()
+{
+    dash(N);
+    cout.fill(' '); cout.width(N - 1); cout << left << "|Прайс-лист" << "|" << endl;
+    dash(N);
+
+    cout << '|'; center("Наименование товара", 20); cout << '|';
+    center("Тип товара", 10); cout << '|';
+    center("Цена за 1 шт (грн)", 16); cout << '|';
+    center("Минимальное количество", 22); cout << '|';
+    center("Дата", 12); cout << '|' << endl;
+    dash(N);
+}
+
+void drawAdress()
+{
+    node* current = head->next;
+    cout << "head[1] = " << head << endl;
+    for (int i = 2; i <= countItem; i++)
+    {
+        cout << "node[" << i << "] = " << current << endl;
+        current = current->next;
+    }
+}
+
+void drawList()
+{
+    drawHat();
+    node* current = head;
+    for (int i = 0; i < countItem; i++)
+    {
+        cout << left << "|"; cout.width(21); cout << current->data.product; cout << "|";
+        cout.width(12); cout << current->data.type; cout << "|";
+        cout.width(20); cout.precision(5); cout << fixed << current->data.price; cout << "|";
+        cout.width(24); cout << left << current->data.min; cout << "|";
+        drawDate(current->data.date.day, current->data.date.month, current->data.date.year, 12);
+        cout << endl; dash(N);
+        current = current->next;
+    }
+    cout.fill(' '); cout.width(N - 1); cout << left << "|Примечание: К – канцтовары, О – оргтехника" << "|" << endl;
+    dash(N);
+    drawAdress();
+}
+
+void deleteList()
+{
+    node* current = head;
+    node* temp;
+    for (int i = 0; i < countItem; i++)
+    {
+        temp = current->next;
+        delete[] current;
+        current = temp;
     }
 }
 
@@ -179,50 +188,17 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    struct Record records[10];
+    Record records[10];
+    records[0] = { "Папка","К",4.75,4,{3,7,2022} };
+    records[1] = { "Бумага","К",13.9,10,{3,4,2021} };
+    records[2] = { "Калькулятор","О",411,1,{4,3,2022} };
 
-    records[0] = { "Баландин", 'С', 123.7000, 2, {6,6,2022} };
-    records[1] = { "Шишков", 'Ш', 79.9800, 3, {7,7,2022} };
-    records[2] = { "Кравченко", 'Д', 134.8000, 1, {7,6,2022} };
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 3; j++)
+            addItem(records[j]);
 
-    //Input(records);
-
-    //Rusakov(records);
-
-    //Table(records);
-
-    Record* A;
-    int count_of_lines = 3;
-
-    A = (Record*)calloc(count_of_lines, sizeof(Record));
-    if (A == NULL) {
-        cout << "OS didn't give memory";
-        exit(1);
-    }
-    for (int i = 0; i < count_of_lines; i++) {
-        //A[i].surname = records[i].surname;
-        strcpy_s(A[i].surname, 10, records[i].surname);
-        A[i].code = records[i].code;
-        A[i].count = records[i].count;
-        A[i].place = records[i].place;
-        A[i].date = records[i].date;
-    }
-
-    Record* tmp = (Record*)realloc(A, 10 * sizeof(Record));
-    A = tmp;
-
-    Record** B;
-    B = (Record**) new Record * [10];
-    for (int i = 0; i < 10; i++) {
-        B[i] = (Record*) new Record;
-        *(B[i]) = records[i];
-    }
-
-
-    for (int i = 0; i < 10; i++) {
-        cout << "Адрес A[i]: " << &A[i] << "A[i]:->surname " << A[i].surname << endl;
-    }
-
-    delete[] B;
-    free(A);
+    drawList();
+    insertMonth();
+    drawList();
+    deleteList();
 }
